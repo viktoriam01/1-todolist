1ef7b44a64fab912fa3d8d6d5dda9959a7459d92
viktoriam01 <aislingdar.com>
2023-01-26 22:13:10 +0800

    feat: add lesson3: addTask by click and by button, refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from "./Todolist";
+import {v1} from "uuid";
 // для фильтрации тасков и кнопки
 export type FilterValuesType = "all" | "active" | "completed"
 
@@ -10,18 +11,28 @@ function App() {
 
     // создание useState c initial array  внутри. useState - функция,в которой первый элемент -массив данных,
     // а второй эл - функция. которая их изменяет
-    const [tasks, setTasks] = useState([
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'ReactJS', isDone: false}
+    const [tasks, setTasks] = useState<Array<TaskType>>([
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'ReactJS', isDone: false}
     ])
 // const tasks2 = [
     //     { id: 1, title: 'Hello world', isDone: true},
     //     { id: 2, title: 'I am happy', isDone: false},
     //     { id: 3, title: 'Yo', isDone: false}
 
+    // добавление таски
+    const addTask = (title: string) => {
+        const newTask: TaskType = {
+            id: v1(),
+            title: title,
+            isDone: false,
+        }
+        setTasks([newTask, ...tasks])
+    }
+
     // удаление таски
-    const removeTask = (taskId: number) => {
+    const removeTask = (taskId: string) => {
         //setTasks работает асинхронно
         setTasks(tasks.filter(t => t.id !== taskId))
         console.log(tasks)
@@ -42,9 +53,9 @@ function App() {
     const getFilteredTasksForRender = () => {
         switch(filter) {
             case "active":
-                return tasks.filter(t => t.isDone === false)
+                return tasks.filter(t => !t.isDone)
             case "completed":
-                return tasks.filter(t => t.isDone === true)
+                return tasks.filter(t => t.isDone)
             default:
                 return tasks
         }
@@ -55,6 +66,7 @@ function App() {
     return (
         <div className="App">
             <Todolist
+                addTask={addTask}
                 removeTask={removeTask}
                 changeFilter = {changeFilter}
                 title={title}
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,48 +1,80 @@
-import React from "react";
+import React, {ChangeEvent, useState, KeyboardEvent} from "react";
 import {FilterValuesType} from "./App";
 
 export type TodolistPropsType = {
     title: string,
     tasks: Array<TaskType>,
-    removeTask: (taskId: number) => void,
+    addTask: (title: string) => void,
+    removeTask: (taskId: string) => void,
     changeFilter: (filter: FilterValuesType) => void
 }
 
 export type TaskType = {
-    id: number,
+    id: string,
     title: string,
     isDone: boolean
 }
 
 export const Todolist = (props: TodolistPropsType) => {
+    //хранилище для инпута
+    const [title,  setTitle] = useState<string> ("")
+
     //условный рендеринг - возвращается тот или иной компонент в зависимости от условия
     const tasksItem = props.tasks.length
     ? props.tasks.map((task: TaskType) => {
+        const onClickRemoveTaskHandler = () => props.removeTask(task.id)
         return (
             //key={task.id} - чтобы лишки идентифицировать для разных целей
             <li key={task.id}>
                 <input type="checkbox" checked={task.isDone}/>
                 <span>{task.title}</span>
-                <button onClick={() => props.removeTask(task.id)}>x</button>
+                <button onClick={onClickRemoveTaskHandler}>x</button>
             </li>
         )
     })
     : <span>Tasks list empty</span>
 
+    // функция-обработчик для добавления таски по клику на кнопку
+    const onClickAddTaskToTodolistHandler = () => {
+        props.addTask(title)
+        setTitle("")
+    }
+
+    //функция-обработчик для добавления значения инпута (названия таски) в локал стейт
+    const onChangeSetLocalTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+
+    // функция-обработчик для добавления таски по нажатию клавиши
+    // то же самое, что и onKeyDown={(e) => {if(e.key === 'Enter') addTask()}}, только использован синтаксис - логическое И, псевдо Иф
+    const onKeyDownAddTaskToTodolistHandler = (e: KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && onClickAddTaskToTodolistHandler()
+
+    //функции-обработчики для фильтров
+    // const onClickSetAllFilterHandler  = () => props.changeFilter('all')
+    // const onClickSetActiveFilterHandler  = () => props.changeFilter('active')
+    // const onClickSetCompletedFilterHandler  = () => props.changeFilter('completed')
+    // можем в таком виде оставить и вызвать в кнопках-фильтрах, или сделать интереснее - создать функци, которая будет генерить
+    //эти функции с нужным фильтром - и уже ее загнать в кнопки-фильтры. (этот вариант и использован ниже)
+    const getSetFilterHandler = (filter: FilterValuesType) => () => props.changeFilter(filter)
+
+
+    //возвращаемый jsx
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input
+                    value = {title}
+                    onChange={onChangeSetLocalTitleHandler}
+                    onKeyDown={onKeyDownAddTaskToTodolistHandler}
+                />
+                <button onClick={onClickAddTaskToTodolistHandler}>+</button>
             </div>
             <ul>
                 {tasksItem}
             </ul>
             <div>
-                <button onClick={() => props.changeFilter('all')}>All</button>
-                <button onClick={() => props.changeFilter('active')}>Active</button>
-                <button onClick={() => props.changeFilter('completed')}>Completed</button>
+                <button onClick={getSetFilterHandler("all")}>All</button>
+                <button onClick={getSetFilterHandler("active")}>Active</button>
+                <button onClick={getSetFilterHandler("completed")}>Completed</button>
             </div>
         </div>
     )
